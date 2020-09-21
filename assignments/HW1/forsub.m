f18717943d8e943594d25e5d68c5e55983c8a086
goodp1 <70656322+goodp1@users.noreply.github.com>
2020-09-20 21:18:36 -0400

    problem 1 finish

--- assignments/HW1/forsub.m
+++ assignments/HW1/forsub.m
@@ -12,12 +12,14 @@ Awork=cat(2,A,b);          %This is our working version of the matrix used to pe
 for ir1=2:nref                                           %loop over rows from 2 to n performing elimination, this index marks what row we are starting the elimination from (i.e. using) for this particular column
     for ir2=ir1:nref                                     %this index marks the present position where elimination is being performed - i.e. where we are applying the elementary row operations
         fact=Awork(ir2,ir1-1);                                    %multiplier of the variable we are attempting to eliminate, its ir-1 column of this row
-        Awork(ir2,:)=Awork(ir2,:)-fact/Awork(ir1-1,ir1-1).*Awork(ir1-1,:);    %subtract off previous row modified by a factor that eliminates the ir-1 column term in this row (so it has only super-diagonal elements), this is a little bit wasteful as it uses entire row...
+        Awork(ir2,:)=Awork(ir2,:)-fact/Awork(ir1-1,ir1-1).*Awork(ir1-1,:); 
+        %disp('Awork = ')
+        %disp(Awork)%subtract off previous row modified by a factor that eliminates the ir-1 column term in this row (so it has only super-diagonal elements), this is a little bit wasteful as it uses entire row...
     end %for
 end %for
 
-disp('elim([Aref,bref]) = ');
-disp(Awork);
+%disp('elim([Aref,bref]) = ');
+%disp(Awork);
 
 
 end
