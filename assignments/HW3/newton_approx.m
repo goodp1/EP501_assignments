b363d99ceaec5abf1bb8949b59ff9ceb64b428c8
goodp1 <70656322+goodp1@users.noreply.github.com>
2020-10-14 22:14:16 -0400

    update

--- assignments/HW3/newton_approx.m
+++ assignments/HW3/newton_approx.m
@@ -1,37 +1,37 @@
-function [root,it,success]=newton_exact(f,fprime,x0,maxit,tol,verbose)
+function [root,it,success]=newton_approx(f,x0,maxit,tol,verbose)
 
 % root=newton_exact(f,fprime)
 %
 % finds a set of roots corresponding to the function f (input as a handle)
 % given a function which computes the derivative
 
-%% Error checking of input and setting of default values
-narginchk(3,6);   %check for correct number of inputs to function
-if (nargin<4)
-    maxit=100;       %maximum number of iterations allowed
-end %if
-if (nargin<5)
-    tol=1e-6;        %how close to zero we need to get to cease iterations
-end %if
-if (nargin<6)
-    verbose=false;
-end %if
+% Error checking of input and setting of default values
+% narginchk(3,6);   %check for correct number of inputs to function
+% if (nargin<4)
+%     maxit=100;       %maximum number of iterations allowed
+% end %if
+% if (nargin<5)
+%     tol=1e-6;        %how close to zero we need to get to cease iterations
+% end %if
+% if (nargin<6)
+%     verbose=false;
+% end %if
 
 
-%% Make sure we don't start at an inflection point with zero derivative
-if (abs(fprime(x0))<tol)
-    warning(' Attempting to start Newton iterations near an inflection point, you may wish to restart with a different guess...');
-    x0=x0+1;   %bump the guess a ways off of initial value to see if we can get anything sensible
-end %if
+% Make sure we don't start at an inflection point with zero derivative
+% if (abs(fprime(x0))<tol)
+%     warning(' Attempting to start Newton iterations near an inflection point, you may wish to restart with a different guess...');
+%     x0=x0+1;   %bump the guess a ways off of initial value to see if we can get anything sensible
+% end %if
 
 
-%% Newton iterations
+% Newton iterations
 it=1;
 root=x0;
 fval=f(root);
 converged=false;
 while(~converged && it<=maxit)
-    derivative=fprime(root);
+    derivative=(f(root+tol)-fval)/tol; %approximate derrivitive 
     if (abs(derivative)<100*tol)    %this (inflection point) will end up kicking the root really far away...
         converged=false;
         warning(' Derivative close to zero, terminating iterations with failed convergence... ');
